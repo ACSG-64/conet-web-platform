ce9568f73978eaddc64cc08a9e8488425461ab92
Andrés Sarmiento <acamilo707@gmail.com>
2025-02-12 19:42:46 -0500

    Supported languages and time constants have been defined

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,5 @@
+import languages from './languages.json';
+import timezones from './timezones.json';
+
+export const constantsFolder = import.meta.dirname;
+export { languages, timezones };
---
+++ src/lib/constants/timezones-creator.ts
@@ -0,0 +1,12 @@
+import fs from 'fs/promises';
+import { constantsFolder } from '.';
+import path from 'path';
+
+const timezones = Intl.supportedValuesOf('timeZone');
+const timezonesJson = timezones.map((tzIdentifier, i) => ({ id: i + 1, tzIdentifier }));
+
+const targetFile = path.resolve(constantsFolder, 'timezones.json');
+
+console.log('Writing...');
+await fs.writeFile(targetFile, JSON.stringify(timezonesJson, null, 4));
+console.log(`Done! Output file: ${targetFile}`);
